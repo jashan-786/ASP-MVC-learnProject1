6724c902bcf9f1a18c1a660d20929a5537625990
Jashan <jashanpreet28122001@gmail.com>
2023-04-21 12:04:29 -0500

    basic mvc learning app completed

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,41 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {   
+
+        private static  List<DogViewModel> dogs=new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel()
+            {
+                Name = "zip",
+                Age = 1
+            };
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog( DogViewModel dogviewModel)
+        {
+            // return View("Index" );
+            dogs.Add(dogviewModel);
+            return RedirectToAction(nameof(Index)); 
+        }
+
+        
+
+        public String Hello()
+        {
+            return "whoese there?";
+
+        }
+    }
+}
